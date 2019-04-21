e6ad7693d38e2c53b45e85427484d76f4cbcdc5e
Yugo Osano <yugo.osano@gmail.com>
2019-04-21 14:50:24 +0900

    trait_example.rs was created.

---
+++ trait_example.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+struct Circle {
+	x: f64,
+	y: f64,
+	radius: f64,
+}
+
+trait HasArea {
+	fn area(&self) -> f64;
+}
+
+impl HasArea for Circle {
+	fn area(&self) -> f64 {
+		std::f64::consts::PI * (self.radius * self.radius)
+	}
+}
+
+fn main () {
+	let c = Circle {
+		x: 0.0f64,
+		y: 0.0f64,
+		radius: 1.0f64,
+	};
+	println!("Area is: {}", c.area()); 
+}
+
