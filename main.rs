2a2a76628b7778099268d7ccb3f4ef4155c5e301
Yugo Osano <yugo.osano@gmail.com>
2019-05-06 23:32:51 +0900

    pointer and vector were practiced in Rust.

--- main.rs
+++ main.rs
@@ -1,8 +1,13 @@
-use std::io;
+//use std::io;
 
 fn main (){
 	println!("Hello, World!");
-	
+	let c1 = 'A';
+	let c1_ptr: *const char = &c1;
+	println!("Hello, pointer {:?}", c1_ptr);
+
+	let v2: std::vec::Vec<f64> = vec![0.0, -1.0, 1.0];
+	assert_eq!(v2.len(), 3);
 }
 
 
